f99c07b3ecc500ce59871c0ea81c86535ee9edf6
Theodora Antonache <theodoraantonache@gmail.com>
2025-03-30 21:02:59 +0300

    Task 1: test.c & makefile

--- test.c
+++ test.c
@@ -1,7 +1,102 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
-int main()
+//vecini
+int dx[]={-1,-1,-1,0,0,1,1,1};
+int dy[]={-1,0,1,-1,1,-1,0,1};
+
+int nrvecinivii(char **m,int N,int M,int x,int y)
 {
-    printf("hello\n");
-    return 0;
+  int ct=0;
+  int nx,ny;
+  for(int i=0;i<8;i++)
+  {
+    nx=x+dx[i];
+    ny=y+dy[i];
+    if(nx>=0 && nx<N && ny>=0 && ny<M && m[nx][ny]=='X')
+      ct++;
+  }
+  return ct;
+}
+
+//copiaza matr sursa(I) in destinatie(F)
+void copiazamatr(char **F,char **I,int N)
+{
+  for(int i=0;i<N;i++)
+    strcpy(F[i],I[i]);
+}
+
+
+
+int main(int argc,char *argv[]) 
+{
+    FILE *fin=fopen(argv[1],"rt");
+    FILE *fout=fopen(argv[2],"wt");
+
+    int task,N,M,K;
+    int vii;
+    fscanf(fin,"%d",&task);
+    fscanf(fin,"%d %d",&N,&M);
+    fscanf(fin,"%d",&K);
+
+    char **mi=malloc(N*sizeof(char *));//matricea initiala
+    char **mf=malloc(N*sizeof(char *));//matricea finala
+    for(int i=0;i<N;i++)
+    {
+      mi[i]=malloc((M+1)*sizeof(char));
+      mf[i]=malloc((M+1)*sizeof(char));
+    }
+
+    for(int i=0;i<N;i++)
+      fscanf(fin,"%s",mi[i]);//citeste in stare initiala
+
+    // Scrie generația 0 în fișierul de output
+    for (int i = 0; i < N; i++)
+    fprintf(fout, "%s\n", mi[i]);
+
+    if (K > 0)
+    fprintf(fout, "\n");
+
+    for(int k=0;k<K;k++)
+    {
+      for(int i=0;i<N;i++)
+      {
+        for(int j=0;j<M;j++) 
+        {
+            vii=nrvecinivii(mi,N,M,i,j);
+            if(mi[i][j]=='X')
+            {
+              if(vii==2 || vii==3) mf[i][j]='X';
+              else mf[i][j]='+';
+            }
+            else
+            {
+              if(vii==3) mf[i][j]='X';
+              else mf[i][j]='+';
+            }
+
+        }
+        mf[i][M]='\0';
+      }
+    
+    for(int i=0;i<N;i++)
+      fprintf(fout,"%s\n",mf[i]);
+    
+    if(k<K-1) fprintf(fout, "\n");
+    copiazamatr(mi,mf,N);
+    
+    }
+    fprintf(fout,"\n");
+for(int i=0;i<N;i++)
+{
+  free(mi[i]);
+  free(mf[i]);
+}
+free(mi);
+free(mf);
+/*
+fclose(fin);
+fclose(fout);*/
+return 0;
 }
